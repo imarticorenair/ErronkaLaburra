dc71beb8fbbd24f474ad5e49c87c4b56176ebdb3
WG32 TALDEA <ik012982i5@H012982.NET>
2024-10-02 11:34:41 +0200

    Button backgrounds

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,9 @@ function displayUpgrades() {
         let button = document.createElement("button");
         button.innerText = `${upgrade.name} (${upgrade.getCost()} gaileta)`;
         button.onclick = () => buyUpgrade(upgrade);
+
+        // Asignar un ID o clase basado en el nombre del upgrade
+        button.id = upgrade.name; // Esto establece el ID del botón
         upgradeButtonsDiv.appendChild(button);
     }    
 }
@@ -42,3 +45,50 @@ setInterval(() => {
 }, 1000);
 
 displayUpgrades();
+
+/*
+// COCKIE-A HASIERATU
+let cookie = new Cookie();
+
+// HOBEKUNTZAK HASIERATU
+let upgrades = [
+    new Upgrade("Kurtsorea", 10, 0.1),
+    new Upgrade("Amona", 100, 10),
+    new Upgrade("Baserria", 1000, 20),
+    new Upgrade("Meatze", 5000, 100)
+];
+
+function displayUpgrades() {
+    let upgradeButtonsDiv = document.getElementById("upgradeButtons");
+    upgradeButtonsDiv.innerHTML = '';
+    for (let upgrade of upgrades) {
+        let button = document.createElement("button");
+        button.innerText = `${upgrade.name} (${upgrade.getCost()} gaileta)`;
+        button.onclick = () => buyUpgrade(upgrade);
+        upgradeButtonsDiv.appendChild(button);
+    }    
+}
+
+function addCookie() {
+    cookie.addCookie();
+}
+
+function buyUpgrade(upgrade) {
+    let cost = upgrade.getCost();
+    if (cookie.cookieCount >= cost) {
+        cookie.cookieCount -= cost;
+        cookie.productionRate += upgrade.buy();
+        cookie.updateDisplay();
+        displayUpgrades();
+        alert(`${upgrade.name} erosi duzu!`);
+    } else {
+        alert("Ez duzu nahikoa gailetarik!");
+    }
+}
+
+setInterval(() => {
+    cookie.produceCookies();
+}, 1000);
+
+displayUpgrades();
+*/
